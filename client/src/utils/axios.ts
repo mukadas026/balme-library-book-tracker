93754a393cb4449554cdad7e1563003ec505ed0a
mukadas026 <mukadasm026@gmail.com>
2024-07-17 17:32:18 +0000

    add netlify redirects

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const client = axios.create({
     // baseURL: "http://localhost:7000/api",
-    baseURL: "https://balme-library-book-tracker.onrender.com:7000/api",
+    baseURL: "https://balme-library-book-tracker.onrender.com/api",
     withCredentials:true,  
 })
