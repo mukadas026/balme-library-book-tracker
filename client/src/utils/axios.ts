5d2ee78fbd58e6bcb16bc447b50166ed0aaac0b5
mukadas026 <mukadasm026@gmail.com>
2024-07-17 17:27:40 +0000

    update server url

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-    baseURL: "http://localhost:7000/api",
-    withCredentials:true,
-    
+    // baseURL: "http://localhost:7000/api",
+    baseURL: "https://balme-library-book-tracker.onrender.com:7000/api",
+    withCredentials:true,  
 })
